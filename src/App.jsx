2c620e6f1d32e094445b0f540f79c7f02b00515b
okemik <okemik@yahoo.com>
2025-06-24 00:33:26 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import SearchBox from "./components/SearchBox/SearchBox";
 
 const LOCAL_STORAGE_KEY = "contacts";
 
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
 function App() {
   const [contacts, setContacts] = useState(() => {
     const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
